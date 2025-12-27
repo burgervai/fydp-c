5b8c5dcb6bcf6d75f4f2dcd2e89b4f10e866f780
burgervai <88193069+burgervai@users.noreply.github.com>
2025-12-27 23:48:35 +0600

    Refactor User model and password hashing logic

--- BACKEND/models/User.js
+++ BACKEND/models/User.js
@@ -1,59 +1,27 @@
-// models/User.js
 const bcrypt = require('bcryptjs');
 
 module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define(
-    'User',
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
-        }
-      },
-      password: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      role: {
-        type: DataTypes.ENUM('patient', 'doctor', 'admin'),
-        defaultValue: 'patient'
-      }
-    },
-    {
-      tableName: 'users',
-      timestamps: true,
-      underscored: true,
-      hooks: {
-        beforeCreate: async (user) => {
-          if (user.password) {
-            user.password = await bcrypt.hash(user.password, 10);
-          }
-        },
-        beforeUpdate: async (user) => {
-          if (user.changed('password')) {
-            user.password = await bcrypt.hash(user.password, 10);
-          }
-        }
+  const User = sequelize.define('User', {
+    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    username: { type: DataTypes.STRING, allowNull: false, unique: true },
+    email: { type: DataTypes.STRING, allowNull: false, unique: true },
+    password: { type: DataTypes.STRING, allowNull: false },
+    role: {
+      type: DataTypes.ENUM('patient', 'doctor', 'admin'),
+      defaultValue: 'patient'
+    }
+  }, {
+    tableName: 'users',
+    underscored: true,
+    hooks: {
+      beforeCreate: async user => {
+        user.password = await bcrypt.hash(user.password, 10);
       }
     }
-  );
+  });
 
-  // Instance method
-  User.prototype.comparePassword = async function (candidatePassword) {
-    return bcrypt.compare(candidatePassword, this.password);
+  User.prototype.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
   };
 
   return User;
