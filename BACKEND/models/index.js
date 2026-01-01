05d9c68f9371964b2d3177a17ad46ecd7a3b5ae9
burgervai <88193069+burgervai@users.noreply.github.com>
2026-01-02 01:43:11 +0600

    Refactor Sequelize model imports and relationships

--- BACKEND/models/index.js
+++ BACKEND/models/index.js
@@ -1,57 +1,57 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
+  dialect: "postgres",
   logging: false,
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
-    }
-  }
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 const db = {};
 
-db.User = require('./User')(sequelize, DataTypes);
-db.Hospital = require('./Hospital')(sequelize, DataTypes);
-db.Doctor = require('./DoctorModel')(sequelize, DataTypes);
-db.Patient = require('./PatientModel')(sequelize, DataTypes);
-db.Appointment = require('./AppointmentModel')(sequelize, DataTypes);
-db.Medicine = require('./MedicineModel')(sequelize, DataTypes);
-db.PatientRecord = require('./PatientRecordModel')(sequelize, DataTypes);
-
-
+db.User = require("./User")(sequelize, DataTypes);
+db.Hospital = require("./Hospital")(sequelize, DataTypes);
+db.Doctor = require("./Doctor")(sequelize, DataTypes);
+db.Patient = require("./Patient")(sequelize, DataTypes);
+db.Appointment = require("./Appointment")(sequelize, DataTypes);
+db.PatientRecord = require("./PatientRecord")(sequelize, DataTypes);
+db.Medicine = require("./Medicine")(sequelize, DataTypes);
 
 /* ================== RELATIONSHIPS ================== */
 
 // Hospital
-db.Hospital.hasMany(db.Doctor);
-db.Hospital.hasMany(db.Patient);
+db.Hospital.hasMany(db.Doctor, { foreignKey: "hospital_id" });
+db.Hospital.hasMany(db.Patient, { foreignKey: "hospital_id" });
 
 // User
-db.User.hasOne(db.Doctor);
-db.User.hasOne(db.Patient);
+db.User.hasOne(db.Doctor, { foreignKey: "user_id" });
+db.User.hasOne(db.Patient, { foreignKey: "user_id" });
 
 // Doctor & Patient
-db.Doctor.belongsTo(db.User);
-db.Patient.belongsTo(db.User);
-db.Doctor.belongsTo(db.Hospital);
-db.Patient.belongsTo(db.Hospital);
+db.Doctor.belongsTo(db.User, { foreignKey: "user_id" });
+db.Patient.belongsTo(db.User, { foreignKey: "user_id" });
+
+db.Doctor.belongsTo(db.Hospital, { foreignKey: "hospital_id" });
+db.Patient.belongsTo(db.Hospital, { foreignKey: "hospital_id" });
 
 // Appointment
-db.Doctor.hasMany(db.Appointment);
-db.Patient.hasMany(db.Appointment);
-db.Appointment.belongsTo(db.Doctor);
-db.Appointment.belongsTo(db.Patient);
+db.Doctor.hasMany(db.Appointment, { foreignKey: "doctor_id" });
+db.Patient.hasMany(db.Appointment, { foreignKey: "patient_id" });
+
+db.Appointment.belongsTo(db.Doctor, { foreignKey: "doctor_id" });
+db.Appointment.belongsTo(db.Patient, { foreignKey: "patient_id" });
 
 // Patient Records
-db.Patient.hasMany(db.PatientRecord);
-db.PatientRecord.belongsTo(db.Patient);
+db.Patient.hasMany(db.PatientRecord, { foreignKey: "patient_id" });
+db.PatientRecord.belongsTo(db.Patient, { foreignKey: "patient_id" });
 
 // Medicines
-db.PatientRecord.hasMany(db.Medicine);
-db.Medicine.belongsTo(db.PatientRecord);
+db.PatientRecord.hasMany(db.Medicine, { foreignKey: "patient_record_id" });
+db.Medicine.belongsTo(db.PatientRecord, { foreignKey: "patient_record_id" });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
