bc752e3d39702deeb0cc5751a9a2c1f2b2d58e35
burgervai <88193069+burgervai@users.noreply.github.com>
2026-01-02 01:47:40 +0600

    Update AppointmentModel.js

--- BACKEND/models/AppointmentModel.js
+++ BACKEND/models/AppointmentModel.js
@@ -1,13 +1,1 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Appointment', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    appointment_date: { type: DataTypes.DATE, allowNull: false },
-    status: {
-      type: DataTypes.ENUM('pending', 'confirmed', 'completed'),
-      defaultValue: 'pending'
-    }
-  }, {
-    tableName: 'appointments',
-    underscored: true
-  });
-};
+
