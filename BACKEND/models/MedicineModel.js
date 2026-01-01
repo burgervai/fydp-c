26b4b922d7345dc80d80e2f3687375f0fdc0f7a5
burgervai <88193069+burgervai@users.noreply.github.com>
2026-01-02 01:49:12 +0600

    Remove MedicineModel.js file

--- BACKEND/models/MedicineModel.js
+++ BACKEND/models/MedicineModel.js
@@ -1,11 +1,1 @@
-module.exports = (sequelize, DataTypes) => {
-  return sequelize.define('Medicine', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: DataTypes.STRING,
-    dosage: DataTypes.STRING,
-    duration: DataTypes.STRING
-  }, {
-    tableName: 'medicines',
-    underscored: true
-  });
-};
+
